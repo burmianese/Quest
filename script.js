54931ff8cb6c64c4b00971289459ebcc712f4134
burmianese <141603963+burmianese@users.noreply.github.com>
2023-08-24 22:38:02 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const commentForm = document.getElementById('comment-form');
+
+  commentForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const comment = document.getElementById('comment').value;
+
+    if (username && comment) {
+      const response = await fetch('submit_comment.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `username=${encodeURIComponent(username)}&comment=${encodeURIComponent(comment)}`,
+      });
+
+      if (response.ok) {
+        alert('Thank you for your comment!');
+        commentForm.reset();
+      } else {
+        alert('Error sending the comment. Please try again later.');
+      }
+    }
+  });
+});
+
